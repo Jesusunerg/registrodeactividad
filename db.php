2b4c6199e36236a97d0afa8956c4f53d11a46d32
Jesusunerg <112362150+Jesusunerg@users.noreply.github.com>
2022-08-29 00:12:59 -0400

    Add files via upload

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+$conn = mysqli_connect(
+  'b1vt7fbazqgtgda7s4gv-mysql.services.clever-cloud.com',
+  'uujvzvbw7kbr1usb',
+  'eaWHUj7HYDv2uxMXowPF',
+  'b1vt7fbazqgtgda7s4gv'
+) or die(mysqli_erro($mysqli));
+
+?>
